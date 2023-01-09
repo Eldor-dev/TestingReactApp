f82969daafa5bddf408661808d97cd1088c7cc0e
Eldor Azimov <eldorazimov37@gmail.com>
2023-01-09 17:21:10 +0900

    Testing User Interaction & State

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 describe("Greting component", () => {
@@ -13,5 +14,42 @@ describe("Greting component", () => {
 
     const helloWorldElement = screen.getByText("Hello World!");
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders good to see you if the button was NOT clicked", () => {
+    render(<Greeting />);
+
+    const goodToSeeYouElement = screen.getByText("It's good to see you!");
+    expect(goodToSeeYouElement).toBeInTheDocument();
+  });
+
+  test('renders "Changed" if the button was clicked ', () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.getByText("Changed!");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('does not render "good to see you" if the button was clicked ', () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.queryByText("It's good to see you!", {
+      exact: false,
+    });
+    expect(outputElement).toBeNull();
   });
 });
